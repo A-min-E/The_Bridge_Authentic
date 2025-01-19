e9f606b40c531cdfe3ff34316e289bdef118cd90
Amine <wissarmohamedamine@gmail.com>
2025-01-19 23:12:39 +0100

    adding the Header and the Hero pics

---
+++ thebridgeauthentic/src/Components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import headerCSS from "./../Header/Header.module.css";
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+
+function Header(){
+    return (
+        <div className={headerCSS.header_wrapper}>
+            <Swiper className={headerCSS.swiper}>
+                <SwiperSlide>
+                    <div className={`${headerCSS.Header_slide} ${headerCSS.slide1}`}>
+                        <div className={headerCSS.content}>
+                            <small>Lessons & Trips</small>
+                            <h2>Enjoy Your Time by US</h2>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={`${headerCSS.Header_slide} ${headerCSS.slide2}`}>
+                        <div className={headerCSS.content}>
+                            <small>Lessons & Trips</small>
+                            <h2>Enjoy Your Time by US</h2>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className={`${headerCSS.Header_slide} ${headerCSS.slide3}`}>
+                        <div className={headerCSS.content}>
+                            <small>Lessons & Trips</small>
+                            <h2>Enjoy Your Time by US</h2>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    )
+}
+
+export default Header
